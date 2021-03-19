e1ef0734abe3aa0a66af6fb5d50b572dea00346d
Gustav <gullegustav@gmail.com>
2021-03-19 11:36:49 +0100

    Main istanciation fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,10 @@ package com.company;
 public class Main {
 UserInput userInput = new UserInput();
     public static void main(String[] args) {
+
 	// write your code here
         Parent parent = new Parent();
+
         System.out.println(parent.createParent().toString());
     }
 }
