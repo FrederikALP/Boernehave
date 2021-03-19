ee9803c5d7394e4cdbef03e2226c478fa3fe7652
Gustav <gullegustav@gmail.com>
2021-03-19 11:30:50 +0100

    Child Class methods and attributes

--- src/com/company/Child.java
+++ src/com/company/Child.java
@@ -2,4 +2,33 @@ package com.company;
 
 public class Child {
 
+
+    private String firstNameChild;
+    private String lastNameChild;
+    private int ageChild;
+
+    Child() {}
+
+    Child(String firstNameChild, String lastNamechild, int ageChild){
+        this.firstNameChild = firstNameChild;
+        this.lastNameChild = lastNamechild;
+        this.ageChild = ageChild;
+    }
+
+    public String getFirstNameChild() {return firstNameChild;}
+
+    public String getLastNameChild() {return lastNameChild;}
+
+    public int getAgeChild() {return ageChild;}
+
+    public void setFirstNameChild(String newFirstNameChild) {this.lastNameChild = newFirstNameChild;}
+
+    public void setLastNameChild(String newLastNameChild) {this.lastNameChild = newLastNameChild;}
+
+    public void setAgeChild(int newAgeChild) {this.ageChild = newAgeChild;}
+
+    @Override
+    public String toString() {
+        return "Child{" + "firstNameChild='" + firstNameChild + '\'' + ", lastNameChild='" + lastNameChild + '\'' + ", ageChild=" + ageChild + '}';
+    }
 }
